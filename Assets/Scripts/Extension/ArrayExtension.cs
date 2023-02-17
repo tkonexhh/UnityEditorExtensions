934b63af801f4ab4bf1434535584628355574a48
DESKTOP-77O6SI9\Administrator <410830616@qq.com>
2023-02-17 15:23:38 +0800

    playerpref preview

--- Assets/Scripts/Extension/ArrayExtension.cs
+++ Assets/Scripts/Extension/ArrayExtension.cs
@@ -1,8 +1,103 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
-using UnityEngine;
+using System.Text;
 
 public static class ArrayExtension
 {
+    public static T Find<T>(this T[] array, Predicate<T> match)
+    {
+        if (array == null || array.Length == 0 || match == null) return default(T);
+        int size = array.Length;
+        for (int i = 0; i < size; i++)
+        {
+            if (match(array[i])) return array[i];
+        }
+        return default(T);
+    }
 
+    public static T[] FindAll<T>(this T[] array, Predicate<T> match)
+    {
+        if (array == null || array.Length == 0 || match == null) return null;
+        List<T> list = new List<T>();
+        int size = array.Length;
+        for (int i = 0; i < size; i++)
+        {
+            if (match(array[i])) list.Add(array[i]);
+        }
+        return list.ToArray();
+    }
+
+    public static int FindIndex<T>(this T[] array, Predicate<T> match)
+    {
+        if (array == null || array.Length == 0 || match == null) return -1;
+        int size = array.Length;
+        for (int i = 0; i < size; i++)
+        {
+            if (match(array[i])) return i;
+        }
+        return -1;
+    }
+
+    public static bool Exist<T>(this T[] array, Predicate<T> match)
+    {
+        if (array == null || array.Length == 0 || match == null) return false;
+        int size = array.Length;
+        for (int i = 0; i < size; i++)
+        {
+            if (match(array[i])) return true;
+        }
+        return false;
+    }
+
+    public static T[] RemoveAll<T>(this T[] array, Predicate<T> match)
+    {
+        if (array == null || array.Length == 0 || match == null) return null;
+        List<T> list = new List<T>();
+        int size = array.Length;
+        for (int i = 0; i < size; i++)
+        {
+            if (match(array[i])) continue;
+            list.Add(array[i]);
+        }
+        return list.ToArray();
+    }
+
+    /// <summary>
+    /// 遍历
+    /// </summary>
+    /// <param name="action">遍历事件</param>
+    public static T[] ForEach<T>(this T[] self, Action<int, T> action)
+    {
+        for (int i = 0; i < self.Length; i++)
+        {
+            action(i, self[i]);
+        }
+        return self;
+    }
+
+    /// <summary>
+    /// 倒序遍历
+    /// </summary>
+    /// <param name="action">遍历事件</param>
+    public static T[] ForEachReverse<T>(this T[] self, Action<T> action)
+    {
+        for (int i = self.Length - 1; i >= 0; i--)
+        {
+            action(self[i]);
+        }
+        return self;
+    }
+
+    /// <summary>
+    /// 倒序遍历
+    /// </summary>
+    /// <param name="action">遍历事件</param>
+    public static T[] ForEachReverse<T>(this T[] self, Action<int, T> action)
+    {
+        for (int i = self.Length - 1; i >= 0; i--)
+        {
+            action(i, self[i]);
+        }
+        return self;
+    }
 }
--- Assets/Scripts/Extension/TransformExtension.cs
+++ Assets/Scripts/Extension/TransformExtension.cs
@@ -4,27 +4,69 @@ using UnityEngine;
 
 public static class TransformExtension
 {
-    //     public static Transform[] FindChildrenByName(this Transform root, string gameobjectName, bool includeDepth, bool includeInactive)
-    //     {
-    //         if (root == null) return null;
-    //         Transform[] transforms = null;
-    //         if (includeDepth) transforms = root.FindAllInChildren<Transform, Transform>(includeInactive);
-    //         else
-    //         {
-    //             List<Transform> list = new List<Transform>();
-    //             for (int i = 0; i < root.childCount; i++)
-    //             {
-    //                 Transform t = root.GetChild(i);
-    //                 if (t == null) continue;
-    //                 if (!t.gameObject.activeInHierarchy && !includeInactive) continue;
-    //                 list.Add(t);
-    //             }
-    //             transforms = list.ToArray();
-    //         }
-    // #if UNITY_EDITOR
-    //         return transforms.FindAll(x => x.gameObject.name.Trim() == gameobjectName);
-    // #else
-    //         return transforms.FindAll(x => x.gameObject.name == gameobjectName);
-    // #endif
-    //     }
+    public static K[] FindAllInChildren<T, K>(this T root, bool includeInactive = false) where T : Component where K : Component
+    {
+        if (root == null) return null;
+        K[] temp = root.GetComponentsInChildren<K>(includeInactive);
+        return temp.RemoveAll(x => x == root);
+    }
+
+    public static Transform[] FindChildrensByName(this Transform root, string gameobjectName, bool includeDepth, bool includeInactive)
+    {
+        if (root == null) return null;
+        Transform[] transforms = null;
+        if (includeDepth) transforms = root.FindAllInChildren<Transform, Transform>(includeInactive);
+        else
+        {
+            List<Transform> list = new List<Transform>();
+            for (int i = 0; i < root.childCount; i++)
+            {
+                Transform t = root.GetChild(i);
+                if (t == null) continue;
+                if (!t.gameObject.activeInHierarchy && !includeInactive) continue;
+                list.Add(t);
+            }
+            transforms = list.ToArray();
+        }
+#if UNITY_EDITOR
+        return transforms.FindAll(x => x.gameObject.name.Trim() == gameobjectName);
+#else
+        return transforms.FindAll(x => x.gameObject.name == gameobjectName);
+#endif
+    }
+
+    public static Transform FindChildrenByName(this Transform root, string gameobjectName, bool includeDepth, bool includeInactive)
+    {
+        if (root == null) return null;
+        Transform[] transforms = null;
+        if (includeDepth) transforms = root.FindAllInChildren<Transform, Transform>(includeInactive);
+        else
+        {
+            List<Transform> list = new List<Transform>();
+            for (int i = 0; i < root.childCount; i++)
+            {
+                Transform t = root.GetChild(i);
+                if (t == null) continue;
+                if (!t.gameObject.activeInHierarchy && !includeInactive) continue;
+                list.Add(t);
+            }
+            transforms = list.ToArray();
+        }
+#if UNITY_EDITOR
+        var res = transforms.FindAll(x => x.gameObject.name.Trim() == gameobjectName);
+        if (res == null || res.Length == 0) { return null; }
+        return res[0];
+#else
+        var res = transforms.FindAll(x => x.gameObject.name == gameobjectName);
+        if (res == null || res.Length == 0) { return null; }
+        return res[0];
+#endif
+    }
+
+    public static void Reset(this Transform self)
+    {
+        self.localPosition = Vector3.zero;
+        self.localScale = Vector3.one;
+        self.localRotation = Quaternion.identity;
+    }
 }
